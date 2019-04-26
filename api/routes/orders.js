5eb0ed74a1e657abb2b64d2d77fe298fbe115bb8
Ernest Ibeh <ibeh_ernest@yahoo.com>
2019-04-26 10:10:53 +0100

    Added Nodemon and Morgan to handle dev restart and logs. We also handled erros

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-// Get orders
+// Handle incoming GET request to /orders
 router.get('/', (req, res, next) => {
   res.status(200).json({
     message: 'Orders was fetched'
--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+
+// Set up morgan to log files activites
+app.use(morgan('dev'))
 
 const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
 
+// Routes which should handle requests
 app.use('/products', productRoutes)
 app.use('/orders', orderRoutes)
+
+// Handle Error Request that are not part of the defined routes
+app.use((req, res, next) => {
+    const error = new Error('Not found')
+    error.status = 404
+    next(error);
+})
+
+// Handle any kind of errors thrown in this application DB etc.
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
 
 module.exports = app;
