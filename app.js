0fee632be87f9015bec4db2c7cfeb1ce0fc0e1cb
Ernest Ibeh <ibeh_ernest@yahoo.com>
2019-04-26 11:42:34 +0100

    Added Body Parser to handle the body. Also added CORS error handling

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 
 // Set up morgan to log files activites
 app.use(morgan('dev'))
+// Use body parser to format what is sent as feedback from api
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+// Set up Cross Origin Resource Sharing CORS and Handle CORS errors
+app.use((req, res, next) => {
+    res.header(
+        "Access-Control-Allow-Origin",
+        "*"
+    )
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    )
+    if (req.method === 'OPTIONS') {
+        res.header(
+            "Access-Control-Allow-Methods",
+            "PUT, POST, PATCH, DELETE, GET"
+        )
+        return res.status(200).json({})
+    }
+})
 
 const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
